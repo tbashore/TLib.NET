be46e646a7c41b781000a94d7549417a2628e08d
Tyson Bashore <dfunct@gmail.com>
2012-06-25 16:32:16 -0400

    Added Random extension methods.

---
+++ TLib/RandomExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TLib
+{
+    public static class RandomExtensions
+    {
+        public static DateTime NextDateTime(this Random random, DateTime minValue, DateTime maxValue)
+        {
+            if (maxValue < minValue)
+            {
+                throw new ArgumentOutOfRangeException("maxValue", "maxValue must be greater than minValue.");
+            }
+            if (minValue == maxValue)
+            {
+                return minValue;
+            }
+
+            Int64 ticksRange = maxValue.Ticks - minValue.Ticks;
+
+            byte[] buff = new byte[8];
+            random.NextBytes(buff);
+            Int64 randomTicks = Math.Abs(BitConverter.ToInt64(buff, 0));
+
+            return minValue.AddTicks(randomTicks % ticksRange);
+        }
+
+        public static T NextEnum<T>(this Random random)
+        {
+            var values = Enum.GetValues(typeof(T));
+            var index = random.Next(values.Length);
+            return values.Cast<T>().ElementAt(index);
+        }
+
+        public static T NextEnum<T>(this Random random, T minValue, T maxValue)
+        {
+            var values = Enum.GetValues(typeof(T));
+
+            var comparer = Comparer<T>.Default;
+            var tValuesInRange = values.Cast<T>().Where(tValue =>
+                (comparer.Compare(minValue, tValue) <= 0 &&
+                comparer.Compare(tValue, maxValue) <= 0));
+
+            var index = random.Next(tValuesInRange.Count());
+            return tValuesInRange.Cast<T>().ElementAt(index);
+        }
+
+        public static T NextEnum<T>(this Random random, T minValue, T maxValue, T except)
+        {
+            var values = Enum.GetValues(typeof(T));
+
+            var comparer = Comparer<T>.Default;
+            var tValuesInRange = values.Cast<T>().Where(tValue =>
+                (comparer.Compare(minValue, tValue) <= 0 &&
+                comparer.Compare(tValue, maxValue) <= 0) &&
+                comparer.Compare(tValue, except) != 0);
+
+            var index = random.Next(tValuesInRange.Count());
+            return tValuesInRange.Cast<T>().ElementAt(index);
+        }
+    }
+}
---
+++ TLibTests/RandomTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TLib;
+
+namespace TLibTests
+{
+    [TestFixture]
+    public class RandomTests
+    {
+        [Test]
+        public void NextDateTime()
+        {
+            var r = new Random();
+
+            var today = DateTime.Today.Date;
+            var tomorrow = today.AddDays(1);
+
+            for (var i = 0; i < 100; i++)
+            {
+                Assert.That(r.NextDateTime(today, tomorrow), Is.InRange(today, tomorrow));
+                Assert.That(r.NextDateTime(today, today), Is.EqualTo(today));
+            }
+        }
+
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NextDateTimeFails()
+        {
+            var r = new Random();
+
+            var today = DateTime.Today.Date;
+            var tomorrow = today.AddDays(1);
+
+            Console.Out.WriteLine(r.NextDateTime(tomorrow, today));
+        }
+
+        public enum SomeCode
+        {
+            Undefined = 0,
+            First = 1,
+            Middle = 125,
+            Last = 1000
+        }
+
+        [Test]
+        public void NextEnum()
+        {
+            var r = new Random();
+
+            for (var i = 0; i < Enum.GetValues(typeof(SomeCode)).Length * 100; i++)
+            {
+                Assert.That(r.NextEnum<SomeCode>(), Is.InRange(SomeCode.Undefined, SomeCode.Last));
+            }
+        }
+
+        [Test]
+        public void NextEnumInRange()
+        {
+            var r = new Random();
+
+            for (var i = 0; i < Enum.GetValues(typeof(SomeCode)).Length * 100; i++)
+            {
+                Assert.That(r.NextEnum(SomeCode.Undefined, SomeCode.Last), Is.InRange(SomeCode.Undefined, SomeCode.Last));
+                Assert.That(r.NextEnum(SomeCode.First, SomeCode.Last), Is.InRange(SomeCode.First, SomeCode.Last));
+                Assert.That(r.NextEnum(SomeCode.Middle, SomeCode.Middle), Is.EqualTo(SomeCode.Middle));
+            }
+        }
+
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NextEnumInRangeFails()
+        {
+            var r = new Random();
+
+            r.NextEnum(SomeCode.Last, SomeCode.First);
+        }
+
+        [Test]
+        public void NextEnumInRangeExcept()
+        {
+            var r = new Random();
+
+            for (var i = 0; i < Enum.GetValues(typeof(SomeCode)).Length * 100; i++)
+            {
+                Assert.That(r.NextEnum(SomeCode.Undefined, SomeCode.Last, SomeCode.Middle), Is.InRange(SomeCode.Undefined, SomeCode.Last).And.Not.EqualTo(SomeCode.Middle));
+                Assert.That(r.NextEnum(SomeCode.First, SomeCode.Last, SomeCode.Last), Is.InRange(SomeCode.First, SomeCode.Middle));
+            }
+        }
+
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NextEnumInRangeExceptFails()
+        {
+            var r = new Random();
+
+            r.NextEnum(SomeCode.Middle, SomeCode.Middle, SomeCode.Middle);
+        }
+    }
+}
